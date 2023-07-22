1c5e6b210ddaa0bbf0eb25fa06dd69d9f7a74b8f
wdrap <40451505+wdrap@users.noreply.github.com>
2023-07-22 10:42:15 +0000

    Added junit5

--- src/test/java/dev/wdrap/AppTest.java
+++ src/test/java/dev/wdrap/AppTest.java
@@ -1,20 +1,14 @@
 package dev.wdrap;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue(true);
     }
 }
