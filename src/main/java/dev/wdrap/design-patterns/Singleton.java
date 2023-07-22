1bcf80cae9cab0fde19243be56fd914dd36ed805
wdrap <40451505+wdrap@users.noreply.github.com>
2023-07-22 10:43:01 +0000

    Thread save singleton pattern example implementation

---
+++ src/main/java/dev/wdrap/design-patterns/Singleton.java
@@ -0,0 +1,22 @@
+import java.util.Objects;
+
+public class Singleton {
+    private static volatile Singleton singleton;
+
+    private Singleton () {}
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+
+    public int getHash() {
+        return Objects.hash(singleton);
+    }
+}
